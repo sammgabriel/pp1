c42d9528ced80dbc4d7839e19bf9a2aa8f95abc7
Sam Gabriel <sgabriel2@mail.greenriver.edu>
2019-01-04 13:59:30 -0500

    Added a function that calculates the average of the numbers in the array

--- functions.php
+++ functions.php
@@ -11,3 +11,18 @@
 
         echo "<p>The highest number is " . max($myArray) . "</p>";
     }
+
+    function average($myArray) {
+
+        $sum = 0;
+
+        foreach ($myArray as $value) {
+
+            $sum = $sum + $value;
+        }
+
+        $average = $sum / sizeof($myArray);
+
+        echo "The average is " . $average;
+
+    }
