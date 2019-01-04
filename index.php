44237b60912e8be5068b98b4e2f6b1171bce7315
Sam Gabriel <sgabriel2@mail.greenriver.edu>
2019-01-04 13:39:05 -0500

    Changed file extension to php, added an array of numbers, and created a function that prints the numbers in the array

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
+<h1>Pair Program 1</h1>
+
+<?php
+
+    $numbers = array(7,9,8,9,8,8,6);
+
+    function printNumbers($numbers) {
+
+        foreach ($numbers as $number) {
+            echo $number . "<br>";
+        }
+    }
+
+    printNumbers($numbers);
+
+
+
+?>
+
+</body>
+</html>
